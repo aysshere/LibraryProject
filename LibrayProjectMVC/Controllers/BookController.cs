c4ae526249228df074e101837336d017db80c1cb
Kanan Ismayilzada <kenanismayilzade12@gmail.com>
2024-08-14 00:09:14 +0300

    book view

---
+++ LibrayProjectMVC/Controllers/BookController.cs
@@ -0,0 +1,23 @@
+﻿using Entity.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LibrayProjectMVC.Controllers
+{
+    public class BookController : Controller
+    {
+        private readonly ILogger<BookController> _logger;
+        private readonly IBookService _bookService;
+        public BookController(ILogger<BookController> logger, IBookService bookService)
+        {
+            _logger = logger;
+            _bookService = bookService;
+        }
+
+        
+        public async Task<IActionResult> Index()
+        {
+            var books = await _bookService.GetAllBooksAsync();
+            return View(books);
+        }
+    }
+}
